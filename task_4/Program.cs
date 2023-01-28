68be5333d0287c2b8b602033c64cacc12840debd
Denis Prozorov <den.abraca4@gmail.com>
2023-01-28 11:37:31 +0300

    add solution task_4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0;
+Console.WriteLine("Enter the first number");
+Console.WriteLine("a=");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+Console.WriteLine("b=");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+Console.WriteLine("c=");
+int c = int.Parse(Console.ReadLine());
+if(a > b && a > c)
+{  
+      max = max + a;
+      Console.Write("max=");
+      Console.WriteLine(max);      
+}
+if(a > b && a < c)
+{
+      max = max + c;
+      Console.Write("max=");
+      Console.WriteLine(max);
+}
+if(a < b && b > c)
+{      
+      max = max + b;
+      Console.Write("max=");
+      Console.WriteLine(max);
+}
+if(a < b && b < c)
+{      
+      max = max + c;
+      Console.Write("max=");
+      Console.WriteLine(max);
+}
