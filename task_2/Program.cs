add6e761853c28433e198e1c86878f7e7df2d4f3
Denis Prozorov <den.abraca4@gmail.com>
2023-01-28 10:54:50 +0300

    add decition task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var max = 0;
+Console.WriteLine("Enter the first number");
+Console.WriteLine("a=");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+Console.WriteLine("b=");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+      max = max + a;
+      Console.WriteLine("a > b");
+      Console.Write("max=");
+      Console.Write(max);
+}
+else
+{
+       max = max + b;
+      Console.WriteLine("a < b");
+      Console.Write("max=");
+      Console.Write(max);
+}
