49b8c6ce3b3c74859b62fa57e246181a2cce9c15
Denis Prozorov <den.abraca4@gmail.com>
2023-01-28 12:50:01 +0300

    add solution task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿ /*решил усложнить задачу на случай если будет введено отрицательное число (в таком случае нам должно показать все отрицательные числа до -1) */
+Console.Write("your number = ");
+int n = int.Parse(Console.ReadLine());
+if ( n > 0)
+{
+    int i = 2;
+    while (i <= n )
+    {   
+    System.Console.WriteLine(i);
+    i = i + 2;
+    }
+}
+if ( n < 0)
+{
+    int i = -2;
+    while (i >= n )
+    {   
+    System.Console.WriteLine(i);
+    i = i - 2;
+    }
+}
