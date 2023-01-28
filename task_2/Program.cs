c368e89b812b6a70e8b746acca16933df36e4feb
Denis Prozorov <den.abraca4@gmail.com>
2023-01-28 11:53:27 +0300

    add solution task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0;
+int min = 0;
+Console.WriteLine("Enter the first number");
+Console.WriteLine("a=");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+Console.WriteLine("b=");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+      min = min + b; 
+      max = max + a;
+      Console.WriteLine("a > b");
+      Console.Write("max=");
+      Console.WriteLine(max);
+      Console.Write("min=");
+      Console.Write(min);
+}
+else
+{
+      max = max + b;
+      min = min + a;
+      Console.WriteLine("a < b");
+      Console.Write("max=");
+      Console.WriteLine(max);
+      Console.Write("min=");
+      Console.Write(min);
+}
